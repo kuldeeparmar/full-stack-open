674d438e6bd425544004a35ec6ce0b55acdb9441
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2025-02-22 23:33:32 +0530

    completed 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Filter from './Filter'
 
 import Form from './Form'
 
 import Persons from './Persons'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
   
   const [newName, setNewName] = useState('')
   const [newPhoneNumber,setNewPhoneNumber] = useState('')
   const [search,setSearch] = useState('')
 
   const searchPersons = persons.filter(person => JSON.stringify(person).toLowerCase().includes(search))
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(respose => {
+      console.log('response fullfilled')
+      setPersons(respose.data);
+    })
+  },[])
+
+  console.log(`persons ${persons.length} `)
 
   const handleChange = (event) => {
     setNewName(event.target.value);
