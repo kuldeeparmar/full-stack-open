6a1b5707abab87404cc7ea64997e9d2397176f03
Kuldeep Parmar <kuldeepparmar246@gmail.com>
2025-02-23 22:40:37 +0530

    completed 3.3

---
+++ part3/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info' , (request,response) => {
+     const message = `<p>PhoneBook has info of ${persons.length} people</p>
+                      <p>${new Date()}</p>`
+
+    response.send(message);
+})
+
+app.get('/api/persons/:id',(request,response) => {
+    const id  = request.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if(!person){
+        response.statusMessage = "NOT FOUND"
+        response.status(404).end()
+    }
+    else{
+        response.json(person)
+    }
+
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server is running on PORT ${PORT}`)
